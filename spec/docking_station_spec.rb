96a06bec7867f20ed48a3229737cd8fcbf8f3c5d
ElliottZaki <spudnick99@hotmail.com>
2022-03-27 16:04:24 +0100

    Passing feature test for DockingStation, which create a new Bike class object

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,4 +8,9 @@ describe DockingStation do
     expect(subject).to respond_to :release_bike
   end
 
+  it 'releases a working bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
 end
