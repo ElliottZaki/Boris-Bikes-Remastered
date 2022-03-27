6fa675f7aa546ad619c41053ad2e57bff68ae151
ElliottZaki <spudnick99@hotmail.com>
2022-03-27 15:45:29 +0100

    Added a Bike class aswell as a working unit & feature test for a working? method in the Bike class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,11 +2,10 @@ require './lib/docking_station'
 
 describe DockingStation do
    #Rspec one-liner syntax: `it` is not available from within an example (e.g. an `it` block) or from constructs that run in the scope of an example (e.g. `before`, `let`, etc). It is only available on an example group (e.g. a `describe` or `context` block) hence why it is here.
-    # it { is_expected.to respond_to :release_bike} 
+    # it { is_expected.to respond_to :release_bike } 
 
   it 'responds to release_bike method' do
     expect(subject).to respond_to :release_bike
-
   end
 
 end
